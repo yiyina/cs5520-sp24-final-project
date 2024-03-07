884ea90e08612ff4648661d604b70d270cf43498
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-03-07 12:17:51 -0800

    add Colors.js in Shared folder

---
+++ Shared/Colors.js
@@ -0,0 +1,13 @@
+export default {
+    GRAY: '#F0F0F0',
+    DARK_GRAY: '#2F3B5E',
+    MID_GRAY: '#565D7C',
+    LIGHT_GRAY: '#646B82',
+    DARK_RED: '#FF0A2A',
+    MID_RED: '#FF2F41',
+    LIGHT_RED: '#FF5962',
+    WHITE: '#FFFFFF',
+    BLACK: '#000000',
+    LIGHT_YELLOW: '#FFE9D8',
+    DARK_YELLOW: '#FFD8BA',
+};
