b902a502d1256ec5f4aaac3aeefa82f8ca0ee183
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-03-24 19:43:22 -0700

    add AuthStack and AppStack in the App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Colors from './Shared/Colors';
@@ -6,22 +7,50 @@ import Login from './Screens/Login';
 import Register from './Screens/Register';
 import TabNavigation from './Components/TabNavigation';
 
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase-files/FirebaseSetup'
+
 const Stack = createStackNavigator();
 
-export default function App() {
+export default function App({ navigation }) {
+  const [userloggedIn, setUserloggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserloggedIn(true);
+      } else {
+        setUserloggedIn(false);
+      }
+    })
+  }, [])
+
+  const AuthStack = (
+    <>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register}
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerBackTitleVisible: false,
+          headerStyle: { backgroundColor: Colors.TRANSPARENT },
+        }}
+      />
+    </>
+  )
+  const AppStack = (
+    <>
+      <Stack.Screen name="Home" component={TabNavigation} />
+    </>
+  )
+
   return (
     <NavigationContainer style={styles.container}>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Register" component={Register}
-          options={{
-            headerShown: true, 
-            headerTitle: "",
-            headerBackTitleVisible: false,
-            headerStyle: {backgroundColor: Colors.TRANSPARENT},
-          }}
-        />
-        <Stack.Screen name="Home" component={TabNavigation}/>
+      <Stack.Navigator
+        initialRouteName='Login'
+        screenOptions={{ headerShown: false }}
+      >
+        {userloggedIn ? AppStack : AuthStack}
       </Stack.Navigator>
     </NavigationContainer>
   );
