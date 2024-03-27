449e43db275ed4a087ec6d9cd714a5dbbaf00d23
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-03-27 16:46:37 -0700

    change style of Edit profile page

--- Components/EditProfile/EditFields.js
+++ Components/EditProfile/EditFields.js
@@ -1,48 +1,53 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
+import Colors from '../../Shared/Colors'
 
 export default function EditFields({ title, type, setType, editProfilePressed }) {
     const isPassword = title === 'Password';
-    const handleFocus = () => {
-        if (isPassword && editProfilePressed) {
-            setType("");
-        }
-    }
+
+    const inputStyle = editProfilePressed ? styles.editableFieldInput : styles.nonEditableFieldInput;
 
     return (
         <View style={styles.fieldContainer}>
             <Text style={styles.fieldText}>{title} : </Text>
-            {editProfilePressed ? (
-                <TextInput 
-                    style={styles.fieldInput} 
-                    value={type} 
-                    onChangeText={setType}
-                    secureTextEntry={isPassword}
-                    onFocus={handleFocus}
-                />
-            ) : (
-                <Text>{isPassword ? "*********" : type}</Text>
-            )}
+            <TextInput 
+                style={inputStyle} 
+                value={type} 
+                onChangeText={setType}
+                secureTextEntry={isPassword}
+                editable={editProfilePressed}
+                onFocus={isPassword ? () => setType("") : null}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     fieldContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
+        justifyContent: 'end',
         width: '80%',
-        margin: 20,
+        margin: 10,
     },
     fieldText: {
         fontWeight: 'bold',
         fontSize: 16,
     },
-    fieldInput: {
+    editableFieldInput: {
         flex: 1,
-        borderBottomWidth: 1,
-        borderColor: 'black',
-        marginLeft: 10,
-        padding: 5,
+        // borderWidth: 2,
+        // borderColor: Colors.LIGHT_YELLOW,
+        backgroundColor: 'white',
+        marginTop: 5,
+        padding: 15,
+        borderRadius: 5, 
+    },
+    nonEditableFieldInput: {
+        flex: 1,
+        // borderWidth: 2,
+        // borderColor: "#e0e0e0",
+        backgroundColor: '#e0e0e0',
+        marginTop: 5,
+        padding: 15,
+        borderRadius: 5, 
     },
 })
