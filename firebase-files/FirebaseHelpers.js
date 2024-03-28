03258a716c4f8aa5bd18b689fe8e03b80b8d590b
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-03-28 00:57:35 -0700

    deal with password in the future

--- firebase-files/FirebaseHelpers.js
+++ firebase-files/FirebaseHelpers.js
@@ -1,5 +1,4 @@
 import { firestore, auth, storage } from "./FirebaseSetup";
-import { reauthenticateWithCredential, updatePassword } from 'firebase/auth';
 import {
     collection,
     getDoc,
@@ -254,18 +253,4 @@ const FirestoreService = {
 
 }
 
-async function updatePassword(uid, currentPassword, newPassword) {
-    try {
-        const user = auth.currentUser;
-        const credential = EmailAuthProvider.credential(user.email, currentPassword);
-        await reauthenticateWithCredential(user, credential);
-
-        await updatePassword(user, newPassword);
-        console.log("Password updated successfully!");
-    } catch (error) {
-        console.error("Error updating password:", error);
-        throw error;
-    }
-}
-
-export { FirestoreService, updatePassword };
+export default FirestoreService;
