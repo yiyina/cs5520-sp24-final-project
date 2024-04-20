80f499f480f924d66d99d781cbf253505043168c
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-04-19 18:42:51 -0700

    change color set for ENERGY

--- Components/Spin/DefaultColorSet.js
+++ Components/Spin/DefaultColorSet.js
@@ -3,7 +3,7 @@ export default ColorThemes = {
     NEUTRAL: ['#C0C4B6', '#EADFDB', '#AEAFB1', '#D8C2B5', '#F2CFB3'],
     KITTEN: ['#BF6989', '#D9C2AD', '#8C5230', '#A6836F', '#F2F2F2'],
     CUTE: ['#90B8DB', '#4389C4', '#FF677C', '#FDBDCB', '#FFFFFF'],
-    ENERGE: ['#9EBF24', '#F2C84B', '#F2B263', '#F2620F', '#8C1D04'],
+    ENERGE: ['#9EBF24', '#F2C84B', '#F2B263', '#F2620F', '#F4DBBC'],
     CHOCOLATE: ['#A65F37', '#D98D62', '#73341D', '#A66963'],
     WOOD: ['#736656', '#A69580', '#593E25', '#A68263', '#D9B9A7']
 };
