aae533ba1b55806343647efc818a68dfb4ef1f9f
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-04-22 14:16:16 -0700

    modified gallery screen

--- Components/UserGallery.js
+++ Components/UserGallery.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, View, Image, StyleSheet, Text, TouchableOpacity, Alert } from 'react-native';
+import { ScrollView, View, Image, StyleSheet, Text, TouchableOpacity, Alert, Dimensions } from 'react-native';
 import Colors from '../Shared/Colors';
 import { getUpdatedUserData } from '../Shared/updateUserData';
 import FirestoreService from '../firebase-files/FirebaseHelpers';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation, useRoute } from '@react-navigation/native';
-
 
 export default function UserGallery() {
 
@@ -13,19 +12,24 @@ export default function UserGallery() {
   const { gallery } = getUpdatedUserData();
   const navigation = useNavigation();
 
+  const handleImagePress = (image) => {
+    console.log("image: ", image);
+    navigation.navigate('Search', { query: image.location });
+  };
+
   useEffect(() => {
     groupImages(gallery);
   }, [gallery]);
   // console.log("gallery: ", gallery);
   const groupImages = (gallery) => {
     const groups = gallery.reduce((acc, img) => {
-      const date = img.createdAt.toDate().toISOString().slice(0, 10); 
+      const date = img.createdAt.toDate().toISOString().slice(0, 10);
       if (!acc[date]) acc[date] = { images: [], locations: new Set() };
       acc[date].images.push(img);
       acc[date].locations.add(img.location || 'No location');
       return acc;
     }, {});
-  
+
     const sortedGroups = Object.keys(groups).sort((a, b) => b.localeCompare(a)).reduce(
       (obj, key) => {
         obj[key] = {
@@ -35,7 +39,7 @@ export default function UserGallery() {
         return obj;
       }, {}
     );
-  
+
     setGroupedImages(sortedGroups);
   };
 
@@ -67,20 +71,20 @@ export default function UserGallery() {
       <View style={styles.container}>
         {Object.entries(groupedImages).map(([date, data]) => (
           <View key={date} style={styles.dateGroup}>
-            <View style={styles.dateAndIconContainer}>
-              <Text style={styles.dateText}>{date}</Text>
-            </View>
-            <View style={styles.imageGroup}>
+            <Text style={styles.dateText}>{date}</Text>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.imageScrollView}>
               {data.images.map((img) => (
                 <View key={img.id} style={styles.imageContainer}>
-                  <Image source={{ uri: img.url }} style={styles.image} />
-                  <Text style={styles.imageLocationText}>{img.location || 'No location'}</Text>
+                  <TouchableOpacity key={img.id} onPress={() => handleImagePress(img)}>
+                    <Image source={{ uri: img.url }} style={styles.image} />
+                    <Text numberOfLines={1} style={styles.imageLocationText}>{img.location || 'No location'}</Text>
+                  </TouchableOpacity>
                   <TouchableOpacity style={styles.deleteButton} onPress={() => handleDelete(img)}>
                     <Icon name="close" size={20} color="white" />
                   </TouchableOpacity>
                 </View>
               ))}
-            </View>
+            </ScrollView>
           </View>
         ))}
       </View>
@@ -91,60 +95,49 @@ export default function UserGallery() {
 const styles = StyleSheet.create({
   scrollView: {
     width: '100%',
+    marginTop: Dimensions.get('window').height * 0.05,
   },
   container: {
     padding: 10,
   },
   dateGroup: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
     marginBottom: 20,
-  },
-  dateAndIconContainer: {
-    flexDirection: 'column',
-    marginRight: 10,
   },
   dateText: {
     fontSize: 16,
     color: Colors.TEXT_COLOR,
-    width: 60,
     fontWeight: 'bold',
+    marginBottom: 10,
   },
-  imageGroup: {
+  imageScrollView: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start', // Change here to align items at the start
-    marginLeft: 10,
-    marginRight: 20,
   },
   imageContainer: {
-    position: 'relative',
-    margin: 10,
-    alignItems: 'center', // Center items vertically in the container
+    marginRight: 10,
+    width: 120,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: Colors.BLACK,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 5,
   },
   image: {
     width: 120,
     height: 120,
-    borderWidth: 3,
-    borderColor: Colors.WHITE,
   },
   imageLocationText: {
-    marginTop: 5, // Margin to separate the text from the image
     fontSize: 12,
-    color: Colors.TEXT_COLOR,
-    textAlign: 'center', // Center the text under the image
-    width: 120, // Match the width of the image
+    color: Colors.DARK_GRAY,
+    marginTop: 5,
   },
   deleteButton: {
     position: 'absolute',
     right: 0,
     top: 0,
-    padding: 4,
-  },
-  deleteButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 10,
+    padding: 3,
+    backgroundColor: Colors.BORDER_GOLD,
+    opacity: 0.8,
   },
 });
